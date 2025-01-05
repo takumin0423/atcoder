c069b0b053f8485a453e0a3f6558f93a70d14e3e
takumin0423 <takumin0423baske@gmail.com>
2025-01-06 03:44:53 +0900

    abc086 a

---
+++ abc086/src/bin/a.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+      a: usize,
+      b: usize
+    }
+
+    let c = a * b;
+
+    if c % 2 == 0 {
+        println!("Even")
+    } else {
+        println!("Odd")
+    }
+}
